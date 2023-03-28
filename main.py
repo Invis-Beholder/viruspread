9aef52c954b125181fc80bf236ff6f648bb10ddf
Beholder <99876593+Invis-Beholder@users.noreply.github.com>
2023-03-29 00:28:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import numpy
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
@@ -6,18 +5,19 @@ import imageio
 
 #5种状态，0健康，1潜伏，2感染，3康复，4死亡
 #4种策略，0123
+#todo：隔离区程序 infect_possible里写密接者被隔离 move里写隔离完成后人位置random
+# todo：公共场所判定
 
 #People类变量：strategy,round, count, first_infected_count,T_infect,migrant,quarantine
 #self参数是一个指向实例本身的引用，用于访问类中的属性和方法。
 class People(object):
-    def __init__(self,strategy,round=300, count=1000, first_infected_count=3,T_infect=10,migrant=5,quarantine=0):
+    def __init__(self,strategy,round=300, count=1000, first_infected_count=3,T_infect=20,migrant=10,quarantine=0):
         self.strategy=strategy#防控策略
         self.totalround=round#最大迭代次数
         self.count = count#总人数
         self.first_infected_count = first_infected_count#0代病人数量
         self.T_infect=T_infect#感染周期，过了这段时间会死亡或者转为康复
         self._people = np.random.normal(0, 50, (self.count, 2))#这里是各个人的坐标位置
-        self.segregate=0
         self.reset()
         self.y1=[]
         self.y2=[]
@@ -56,31 +56,32 @@ class People(object):
             if self._status[i] == state:
                 continue
             else:
-                self.set_state(i, state)
+                self._status[i] = state
+                # 记录状态改变的时间
+                self._timer[i] = self._round
                 n += 1
 
-    def set_state(self, i, state):
-        self._status[i] = state
-        # 记录状态改变的时间
-        self._timer[i] = self._round
 
     def random_movement(self ):
         """随机生成移动距离，健康人群乱走，感染人群上移向医院方向，感染距离与migrant参数有关
         """
         w=np.random.normal(0, self.migrant, (self.count, 2))
+        for i in range(self.count):
+            if self._status[i]!=2 and self._people[i][0]>250:
+                w[i][0]=np.random.uniform(-15, -25)
         #下面是使感染者向右方医院移动，如果人已经在医院范围内了，x坐标就不动,暂时设定医院区域为x>250的部分
         # 生成和w[self._status==2]一样大小（shape）的数组赋给w[self._status==2]
-        w[self._status == 2, 0] = np.random.uniform(15, 25,size=w[self._status == 2].shape[0])
+        w[self._status == 2, 0] = np.random.uniform(20, 30,size=w[self._status == 2].shape[0])
         condition = np.logical_and(self._status == 2, self._people[:, 0] > 250)
         w[condition, 0] = 0
         #下面是康复者向其他区域定向移动
         dt = self._round - self._timer
         condition=np.logical_and(self._status==3,dt<=7)
-        w[condition, 0] =np.random.uniform(-15, -25, size=w[condition].shape[0])
+        w[condition, 0] =np.random.uniform(-20, -30, size=w[condition].shape[0])
         #隔离区内人不动
         if(self.strategy==3):
             for i in range(self.count):
-                if (self._people[i][0]<-250 and self._people[i][1]<-250):
+                if (self._people[i][0]>350 ):
                     w[i]=0
         return w
 
@@ -125,11 +126,11 @@ class People(object):
         movement[switch == 0] = 0
         #死了就别动
         movement[self._status==4]=0
-
+        #公共场所管控，不允许感染者进入，会把里面的感染者赶出去
         if(self.strategy>1):
             condition = np.logical_and(self._status == 2, self._people[:, 1] >0)
             if(np.size(movement[condition,1])):
-                movement[condition,1]=np.random.uniform(-10, -20)
+                movement[condition,1]=np.random.uniform(-20, -30)
         self._people = self._people + movement
 
     def change_state(self):
@@ -178,7 +179,7 @@ class People(object):
                 inf = self.infected[j]
                 if(self._round-self._timer[j]<self.quarantine):
                     continue
-                if(inf[0]<-230 and inf[1]<-230):
+                if(inf[0]>350):
                     continue
                 dm = (self._people - inf) ** 2
                 d = dm.sum(axis=1) ** 0.5
@@ -191,19 +192,17 @@ class People(object):
             vector = np.vectorize(np.int_)
             a = vector(a)
             for i in a:
-                self._people[i]=np.random.normal(-300, 10, (1, 2))#新的隔离者送入隔离区随机位置
+                self._people[i][0]=np.random.normal(400, 10)#新的隔离者送入隔离区随机位置
                 self._timer[i]=self._round#更新时间状态
-            b=self._quarantinepeople.size
             self._quarantinepeople = np.append(self._quarantinepeople, a)  # 合并新老隔离者
             self._quarantinepeople = np.unique(self._quarantinepeople)  # 去重
-            self.segregate += self._quarantinepeople.size-b
             self._quarantinepeople=vector(self._quarantinepeople)
+            a=len(self._quarantinepeople)
 
+            for i in range(a):#已有的隔离者检查是否已到隔离时间
+                if(self._round - self._timer[self._quarantinepeople[i]]==24 and (self._status[self._quarantinepeople[i]]!=4)):
+                    self._people[self._quarantinepeople[i]]=np.random.normal(0, 50, (1, 2))
 
-            for i in self._quarantinepeople:#已有的隔离者检查是否已到隔离时间
-                if(self._round - self._timer[i]==21 and (self._status[i]==3 or self._status[i]==0)):
-                    self._people[i]=np.random.normal(0, 50, (1, 2))
-                    self.segregate-=1
 
 
 
@@ -247,7 +246,7 @@ class People(object):
         a2.set_ylim([0,1200])
         # x,y都得是数组
         x = np.arange(self._round)
-        self.y1.append(self.segregate-len(self.dead))
+        self.y1.append(np.count_nonzero((self._people[:, 0]>330) ))
         self.y2.append(len(self.infected))
         a2.plot(x, self.y1, 'k.-', x, self.y2, 'r.-')
         a2.legend(["quarantinepeople", "infected"], loc='upper right')
@@ -283,7 +282,7 @@ if __name__ == '__main__':
     a3 = plt.subplot2grid((2, 3), (1, 2), rowspan=1, colspan=1)
     #plt.ion()函数打开交互式模式,后立马显示图片
     plt.ion()
-    p = People(3, round=300, count=1000, first_infected_count=3, T_infect=20, migrant=10, quarantine=0)
+    p = People(0, round=300, count=1000, first_infected_count=3, T_infect=20, migrant=10, quarantine=0)
     #在pause函数，使更新后的图像暂停3s
     plt.pause(3)
 
